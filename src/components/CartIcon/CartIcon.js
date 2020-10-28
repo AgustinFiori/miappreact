93fb49f7311b060696b5f1083857dfd5a919cc39
AgustinFiori <afu_96@hotmail.com>
2020-10-28 00:07:59 -0300

    Iconos

---
+++ src/components/CartIcon/CartIcon.js
@@ -0,0 +1,8 @@
+import React from "react";
+import icon from "./logoafc.png"
+import './iconStyle.css';
+
+const CartIcon = () => <img src ={icon} className = "icono" alt="icono"></img>
+
+export default CartIcon;
+
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,14 @@
 import React from "react";
 import "./NavBar.css"; 
+import CartIcon from "../CartIcon/CartIcon"
 
-function NavBar() {
-  return (
-      <ul>
+const NavBar = () => (
+      <ul><CartIcon />
   <li><a className="active" href="#home">Home</a></li>
   <li><a href="#news">News</a></li>
   <li><a href="#contact">Contact</a></li>
   <li><a href="#about">About</a></li>
-      </ul>
+  </ul>
   );
-}
 
 export default NavBar;
