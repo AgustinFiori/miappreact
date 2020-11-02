a890c78fe406208f0a12d232c96bdc05cb8e2567
AgustinFiori <afu_96@hotmail.com>
2020-11-02 06:35:33 -0300

    contador js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar.js";
 import Saludo from "./components/Home/home.js"
 
 function App() {
+  const [count, setCount] = useState(0)
   return (
     <div className="App">
       <NavBar />
@@ -11,6 +12,24 @@ function App() {
         <p><Saludo tutor = "Gonzalo" /></p>
         <p><Saludo tutor = "Agustin" /></p>
       </header>
+
+      <button onClick={() => {
+        if (count === 10) {
+          setCount(10);
+          return
+        }
+        setCount(count+1)}}
+
+      >+</button><p>{count}</p>
+      
+      <button onClick={() => {
+        if (count === 0) {
+          setCount(0);
+          return
+        }
+        setCount(count-1)}}
+
+      >-</button>
     </div>
   );
 }
